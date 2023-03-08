96675b9669a499e526b4964eb5273231a2fcbf86
DanielSantiagoMarti <125405685+DanielSantiagoMarti@users.noreply.github.com>
2023-03-07 21:48:40 -0600

    cambio

--- Clasegolosinas.py
+++ Clasegolosinas.py
@@ -134,99 +134,99 @@ elif listadedulces == 2:
 else:
         print('no hay opciones')     
 
-# class golosinas:
-#     def _init_(self,codigo, dulce, precio, caducidad, piezas) -> int:
-#         self.codigo = float(codigo)
-#         self.dulce = dulce
-#         self.precio = int(precio)
-#         self.caducidad = caducidad
-#         self.piezas = int(piezas)
+ class golosinas:
+     def _init_(self,codigo, dulce, precio, caducidad, piezas) -> int:
+         self.codigo = float(codigo)
+         self.dulce = dulce
+         self.precio = int(precio)
+         self.caducidad = caducidad
+         self.piezas = int(piezas)
 
-#     def agregar(self, cantidad):
-#         self.piezas += cantidad
+     def agregar(self, cantidad):
+         self.piezas += cantidad
 
-#     def vender(self, cantidad):
-#         self.piezas -= cantidad
+     def vender(self, cantidad):
+         self.piezas -= cantidad
 
-#     def almacen(self):
-#         return self.piezas
+     def almacen(self):
+         return self.piezas
     
-#     def getPrecio(self):
-#         return self.precio
+     def getPrecio(self):
+         return self.precio
 
-# class paletas(golosinas):
-#     def _init_(self, codigo,dulce, precio, caducidad, piezas) -> int:
-#         super()._init_(codigo,dulce, precio, caducidad, piezas)
+ class paletas(golosinas):
+     def _init_(self, codigo,dulce, precio, caducidad, piezas) -> int:
+         super()._init_(codigo,dulce, precio, caducidad, piezas)
 
-# class chocolates(golosinas):
-#     def _init_(self, codigo, dulce, precio, caducidad, piezas) -> int:
-#         super()._init_(codigo, dulce, precio, caducidad, piezas)
-#     pass
-
-
-# buba = paletas(1,'bubbalob',2,'10 diciembre 2023', 1000)
-# rocka = paletas(1.1,'reockaleta',3,'10 diciembre 2023', 1000)
-# sonrics = paletas(1.2,'ronricks',4,'10 diciembre 2023', 1000)
-
-# carlosv = chocolates(2,'Carlosv',8,'11 noviembre',600)
-# ferrero = chocolates(2.1,'ferrero Roshe',20,'11 noviembre',600)
-# lapose = chocolates(2.2,'Lapose',7,'11 noviembre',600)
-
-# almacen = {'1.2': buba, 'rocaleta': rocka}
-
-# cantidadDeDulcae = 3
-
-# print(almacen[x].almacen())
-# print(almacen['bubalo'].getPrecio())
-# almacen['bubalo'].vender(3)
-# print(almacen['bubalo'].almacen())
-
-# print(f'Precio total: {cantidadDeDulcae * buba.getPrecio()}')
+ class chocolates(golosinas):
+     def _init_(self, codigo, dulce, precio, caducidad, piezas) -> int:
+         super()._init_(codigo, dulce, precio, caducidad, piezas)
+     pass
 
 
+ buba = paletas(1,'bubbalob',2,'10 diciembre 2023', 1000)
+ rocka = paletas(1.1,'reockaleta',3,'10 diciembre 2023', 1000)
+ sonrics = paletas(1.2,'ronricks',4,'10 diciembre 2023', 1000)
+
+ carlosv = chocolates(2,'Carlosv',8,'11 noviembre',600)
+ ferrero = chocolates(2.1,'ferrero Roshe',20,'11 noviembre',600)
+ lapose = chocolates(2.2,'Lapose',7,'11 noviembre',600)
+
+ almacen = {'1.2': buba, 'rocaleta': rocka}
+
+ cantidadDeDulcae = 3
+
+ print(almacen[x].almacen())
+ print(almacen['bubalo'].getPrecio())
+ almacen['bubalo'].vender(3)
+ print(almacen['bubalo'].almacen())
+
+ print(f'Precio total: {cantidadDeDulcae * buba.getPrecio()}')
 
 
 
-# listadedulces = int(input('\n\n\tescoger la categoria de dulces:\n\n\t1.paletas\n\n\t2.chocolates\n\n\t'))
 
 
-# if listadedulces == 1:
+ listadedulces = int(input('\n\n\tescoger la categoria de dulces:\n\n\t1.paletas\n\n\t2.chocolates\n\n\t'))
+
+
+ if listadedulces == 1:
     
-#     print('\n\n\tlista de paletas: \n\n\t',[{buba.codigo:buba.dulce,rocka.codigo: rocka.dulce,sonrics.codigo: buba.dulce}])
+     print('\n\n\tlista de paletas: \n\n\t',[{buba.codigo:buba.dulce,rocka.codigo: rocka.dulce,sonrics.codigo: buba.dulce}])
 
-#     op2 = float(input('\n\tingrese el codigo de paleta\n\n\n\t'))
+     op2 = float(input('\n\tingrese el codigo de paleta\n\n\n\t'))
                 
-#     if op2 == buba.codigo :
-#         print({buba.codigo: [buba.dulce,buba.precio,buba.caducidad,buba.piezas]})
-#     elif op2 == rocka.codigo:
-#         print({rocka.codigo: [rocka.dulce,rocka.precio,rocka.caducidad,rocka.piezas]})
-#     elif op2 == sonrics.codigo:
-#         print({sonrics.codigo: [sonrics.dulce,sonrics.precio,sonrics.caducidad,sonrics.piezas]})
-#     else:
-#             print('no existe opcion')
+     if op2 == buba.codigo :
+         print({buba.codigo: [buba.dulce,buba.precio,buba.caducidad,buba.piezas]})
+     elif op2 == rocka.codigo:
+         print({rocka.codigo: [rocka.dulce,rocka.precio,rocka.caducidad,rocka.piezas]})
+     elif op2 == sonrics.codigo:
+         print({sonrics.codigo: [sonrics.dulce,sonrics.precio,sonrics.caducidad,sonrics.piezas]})
+     else:
+             print('no existe opcion')
                 
-# elif listadedulces == 2:
-#     print('\n\n\tlista de Chocolate: \n\n\t',[{carlosv.codigo:carlosv.dulce,ferrero.codigo: ferrero.dulce,lapose.codigo: lapose.dulce}])
+ elif listadedulces == 2:
+     print('\n\n\tlista de Chocolate: \n\n\t',[{carlosv.codigo:carlosv.dulce,ferrero.codigo: ferrero.dulce,lapose.codigo: lapose.dulce}])
 
-#     op3 = float(input('\n\tingrese el codigo de paleta\n\t'))
-#     if op3 == carlosv.codigo :
+     op3 = float(input('\n\tingrese el codigo de paleta\n\t'))
+     if op3 == carlosv.codigo :
 
-#         print({carlosv.codigo: [carlosv.dulce,carlosv.precio,carlosv.caducidad,carlosv.piezas]})
-#         # ingrese la cantidad de dulces
-#         # vvariable x
-#         carlosv.vender(x)
-#         # quieres quitar
+         print({carlosv.codigo: [carlosv.dulce,carlosv.precio,carlosv.caducidad,carlosv.piezas]})
+          ingrese la cantidad de dulces
+          vvariable x
+         carlosv.vender(x)
+          quieres quitar
 
-#     elif op3 == ferrero.codigo:
-#         print({rocka.codigo: [ferrero.dulce,ferrero.precio,ferrero.caducidad,ferrero.piezas]})
+     elif op3 == ferrero.codigo:
+         print({rocka.codigo: [ferrero.dulce,ferrero.precio,ferrero.caducidad,ferrero.piezas]})
 
-#     elif op3 == lapose.codigo:
-#         print({sonrics.codigo: [lapose.dulce,lapose.precio,lapose.caducidad,lapose.piezas]})
+     elif op3 == lapose.codigo:
+         print({sonrics.codigo: [lapose.dulce,lapose.precio,lapose.caducidad,lapose.piezas]})
 
-#     else:
-#             print('no existe opcion')
-# else:
-#         print('no hay opciones')}
+     else:
+             print('no existe opcion')
+ else:
+         print('no hay opciones')}
 
 """
     catalogo_precios = {"rocaleta": 10, "bubalo": 2}
